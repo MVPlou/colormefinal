e8ab69d9d5b1de2b4ec580db858acd034169be44
GIT SERVICE NAME (NOT USERNAME) <GIT SERVICE EMAIL>
2024-07-12 10:01:49 +0100

    domain fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['colorme-gg.b-cdn.net'],
-    },
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  images: {
+    domains: ['Colorme-gg.b-cdn.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'Colorme-gg.b-cdn.net',
+        pathname: '/Coloring Pages/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
